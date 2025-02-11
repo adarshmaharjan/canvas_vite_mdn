226c03fcc63df9b6058decc74fdd94ef2e4ee0fc
adarsh maharjan <adarsh986030@gmail.com>
2025-02-11 17:09:18 +0545

    chore: implemented draw pacman funcition

--- src/main.ts
+++ src/main.ts
@@ -361,3 +361,135 @@ function drawHeart() {
 }
 // implement cubic bezier curve
 // drawHeart();
+
+function drawPacman() {
+  function roundedRect({
+    ctx,
+    x,
+    height,
+    radius,
+    width,
+    y,
+  }: {
+    ctx: CanvasRenderingContext2D;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    radius: number;
+  }) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.arcTo(x, y + height, x + height, y + height, radius);
+    ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+    ctx.arcTo(x + width, y, x + width - radius, y, radius);
+    ctx.arcTo(x, y, x, y + radius, radius);
+
+    ctx.stroke();
+  }
+
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) {
+    return;
+  }
+  roundedRect({
+    ctx,
+    x: 12,
+    y: 12,
+    width: 184,
+    height: 168,
+    radius: 15,
+  });
+  roundedRect({
+    ctx,
+    x: 19,
+    y: 19,
+    width: 170,
+    height: 154,
+    radius: 9,
+  });
+
+  roundedRect({ ctx, x: 53, y: 53, width: 49, height: 33, radius: 10 });
+  roundedRect({
+    ctx,
+    x: 53,
+    y: 119,
+    width: 49,
+    height: 16,
+    radius: 6,
+  });
+  roundedRect({
+    ctx,
+    x: 135,
+    y: 53,
+    width: 49,
+    height: 33,
+    radius: 10,
+  });
+  roundedRect({
+    ctx,
+    x: 135,
+    y: 119,
+    width: 25,
+    height: 49,
+    radius: 10,
+  });
+
+  ctx.beginPath();
+  ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+  ctx.lineTo(31, 37);
+  ctx.fill();
+
+  for (let i = 0; i < 8; i += 1) {
+    ctx.fillRect(51 + i * 16, 35, 4, 4);
+  }
+
+  for (let i = 0; i < 6; i += 1) {
+    ctx.fillRect(115, 51 + i * 16, 4, 4);
+  }
+
+  for (let i = 0; i < 8; i += 1) {
+    ctx.fillRect(51 + i * 16, 99, 4, 4);
+  }
+
+  ctx.beginPath();
+  ctx.moveTo(83, 116);
+  ctx.lineTo(83, 102);
+  ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+  ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+  ctx.lineTo(111, 116);
+  ctx.lineTo(106.333, 111.333);
+  ctx.lineTo(101.666, 116);
+  ctx.lineTo(97, 111.333);
+  ctx.lineTo(92.333, 116);
+  ctx.lineTo(87.666, 111.333);
+  ctx.lineTo(83, 116);
+  ctx.fill();
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.moveTo(91, 96);
+  ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+  ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+  ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+  ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+  ctx.moveTo(103, 96);
+  ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+  ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+  ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+  ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+  ctx.fill();
+
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+  ctx.fill();
+}
+
+// draw pacman
+drawPacman();
